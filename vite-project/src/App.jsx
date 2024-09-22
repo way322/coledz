cda1bc0f7f53681cf06d87ef0ea7657f4e109bbc
way322 <iliyamalihin@yandex.ru>
2024-09-23 01:40:06 +0300

    first

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css'
+
+export function App() {
+  let [text, setText] = useState(1)
+  
+    return (
+      <>
+
+      </>
+    )
+  }
+
+export default App
